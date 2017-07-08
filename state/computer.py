c3ece53428c8fe567157cb9e58838e171f7ed629
Jiao Xue <jiao.xue@ihandysoft.com>
2017-07-08 00:37:25 +0000

    a computer in state pattern with python __class__

---
+++ state/computer.py
@@ -0,0 +1,55 @@
+class ComputerState(object):
+
+    name = "state"
+    allowed = []
+
+    def switch(self, state):
+        if state.name in self.allowed:
+            print "Current: {} => switched to new state {}".format(self, state.name)
+            self.__class__ = state
+        else:
+            print "Current: {} => switched to new state {} Not Possible".format(self, state.name)
+
+    def __str__(self):
+        return self.name
+
+
+class Off(ComputerState):
+    name = "off"
+    allowed = ["on"]
+
+
+class On(ComputerState):
+    name = "on"
+    allowed = ["off", "suspend", "hibernate"]
+
+
+class Suspend(ComputerState):
+    name = "suspend"
+    allowed = ["on"]
+
+
+class Hibernate(ComputerState):
+    name = "hibernate"
+    allowed = ["on"]
+
+
+class Computer(object):
+    def __init__(self, model="HP"):
+        self.model = model
+        self.state = Off()
+
+    def change(self, state):
+        self.state.switch(state)
+
+
+if __name__ == '__main__':
+    comp = Computer()
+    comp.change(On)
+    comp.change(Off)
+
+    comp.change(On)
+    comp.change(Suspend)
+    comp.change(Hibernate)
+    comp.change(On)
+    comp.change(Off)
